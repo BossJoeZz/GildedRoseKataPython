cef8f940dc2ac03a2a3c1cb5818c117664fe3fc9
Zheng Qiao <109362533+BossJoeZz@users.noreply.github.com>
2024-10-01 21:22:31 -0700

    Update test_gilded_rose.py

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -11,6 +11,50 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
 
+    def test_vest_item_should_decrease_after_one_day(self):
+        vest = "+5 Dexterity Vest"
+        items = [Item(vest, 1, 2), Item(vest, 9, 19), Item(vest, 4, 6)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        self.assertEqual([(item.sell_in, item.quality) for item in items], [(0, 1), (8, 18), (3, 5)])
+
+    def test_aged_brie_should_increase_in_quality(self):
+        elixir = "Elixir of the Mongoose"
+        items = [Item(elixir, 1, 2), Item(elixir, 3, 4), Item(elixir, 5, 10)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        self.assertEqual([(item.sell_in, item.quality) for item in items], [(1, 2), (3, 2), (5, 6)])
+
+    def test_sulfuras_should_not_change(self):
+        sulfuras = "Sulfuras, Hand of Ragnaros"
+        items = [Item(sulfuras, 1, 2), Item(sulfuras, 5, 2)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        self.assertEqual([(item.sell_in, item.quality) for item in items], [(3, 4), (9, 10)])
+
+    def test_backstage_pass_increases_in_quality(self):
+        backstage = "Backstage passes to a TAFKAL80ETC concert"
+        items = [Item(backstage, 3, 3), Item(backstage, 5, 1), Item(backstage, 2, 3)]
+        gr = GildedRose(items)
+    
+        gr.update_quality()
+
+        self.assertEqual([(item.sell_in, item.quality) for item in items], [(1, 2), (3, 4), (5, 6)])
+
+    def test_backstage_pass_quality_drops_to_zero(self):
+        cake = "Conjured Mana Cake"
+        items = [Item(cake, 1, 2), Item(cake, 3, 4)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        self.assertEqual([(item.sell_in, item.quality) for item in items], [(0, 1), (1, 2)])
 
 if __name__ == '__main__':
     unittest.main()
